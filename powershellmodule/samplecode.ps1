bdfdfb92ef2c981c17394d78bc2f72a4513c6d63
jankidas.parashar@royalmail.com <Application.Devops@royalmail.com>
2024-12-11 21:10:02 +0530

    added

--- powershellmodule/samplecode.ps1
+++ powershellmodule/samplecode.ps1
@@ -1,8 +1,1 @@
-# Define the main entry point of the script
-function Main {
-    # Output a greeting message to the console
-    Write-Host "Hello, World!"
-}
-
-# Call the Main function to execute the script
-Main
+Write-Host "Hello, World!"
